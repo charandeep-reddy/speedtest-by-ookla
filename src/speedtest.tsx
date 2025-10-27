303dfdb6b80c517e2fd2f41c4aa043d00995660e
Charandeep <sangaticherry3306@gmail.com>
2025-10-27 18:49:45 +0530

    feat: add Ookla Speedtest CLI command

--- src/speedtest.tsx
+++ src/speedtest.tsx
@@ -1,5 +1,14 @@
 import { Detail } from "@raycast/api";
+import { execSync } from "child_process";
 
 export default function Command() {
-  return <Detail markdown="# Hello World" />;
+  let output: string;
+
+  try {
+    output = execSync("/opt/homebrew/bin/speedtest --accept-license --accept-gdpr", { encoding: "utf-8" });
+  } catch (error: any) {
+    output = error.message;
+  }
+
+  return <Detail markdown={output} />;
 }
